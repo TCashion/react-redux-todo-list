dda8bf0f4b526b2fa2efbc04673f2b27fb000e5f
TCashion <58757351+TCashion@users.noreply.github.com>
2022-01-17 09:12:41 -0700

    put input value into state

--- src/components/ListInput.jsx
+++ src/components/ListInput.jsx
@@ -1,16 +1,29 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const ListInput = (props) => {
   const { dispatchAddTodo } = props;
   const listInputRef = useRef(null);
 
+  const [inputValue, setInputValue] = useState("");
+
   function handleClick() {
     dispatchAddTodo(listInputRef.current.value);
   }
 
+  function handleInputChange(e) {
+    setInputValue(e.target.value)
+  }
+
   return (
+    // TODO: the input value should live in state
     <div>
-      <input ref={listInputRef} type="text" placeholder="Add a todo" />
+      <input
+        ref={listInputRef}
+        type="text"
+        placeholder="Add a todo"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
       <button onClick={handleClick}>Submit</button>
     </div>
   );
